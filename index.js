b1a4decf7f978466ca69959438c8972c25a449c1
Ernst Chapman <ernstc@awards.co.za>
2020-03-12 10:51:17 +0200

    added hapi swagger

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
-const Hoek = require('@hapi/hoek');
-
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('./package');
+const Joi = require('@hapi/joi');
 
 const init = async () => {
 
@@ -10,6 +13,23 @@ const init = async () => {
         port: 3000,
         host: 'localhost'
     });
+
+    const swaggerOptions = {
+        info: {
+            title: 'Test API Documentation',
+            version: Pack.version,
+        },
+        grouping: 'tags'
+    };
+
+    await server.register([
+        Inert,
+        Vision,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
+    ]);
 
     server.route({
         method: '*',
@@ -29,34 +49,88 @@ const init = async () => {
 
     server.route({
         method: 'GET',
-        path: '/hello/{user}',
-        handler: (request, h) => {
-            const user = request.params.user ? request.params.user : 'stranger';
-            return `Hello ${user}`;
+        path: '/user/{user}',
+        options: {
+            handler: function (request, h) {
+                const user = request.params.user ? request.params.user : 'stranger';
+                return `Hello ${user}`;
+            },
+            description: 'Get user',
+            notes: 'Returns the user name passed in the path',
+            tags: ['api', 'user'], // ADD THIS TAG
+            validate: {
+                params: Joi.object({
+                    user : Joi.string()
+                            .required()
+                            .description('user name'),
+                })
+            }
         }
     });
 
     server.route({
         method: 'GET',
-        path: '/hello/{user*2}',
-        handler: (request, h) => {
-            const userParts = request.params.user.split('/');
-            return `Hello ${userParts[0]} and ${userParts[1]}`;            
+        path: '/user/{user}/age/{age}',
+        options: {
+            handler: (request, h) => {
+                const user = request.params.user;
+                const age = request.params.age;
+                return `Hello ${user} you are ${age} years old`;            
+            },
+            description: 'Get user name and age',
+            notes: 'Returns user name and age passed in the path',
+            tags: ['api', 'user'], // ADD THIS TAG
+            validate: {
+                params: Joi.object({
+                    user : Joi.string()
+                            .required()
+                            .description('user name'),
+                    age : Joi.number()
+                            .required()
+                            .description('user age'),
+                })
+            }
         }
     });
 
     server.route({
         method: 'GET',
         path: '/object',
-        handler: function (request, h) {
-            const user = {
-                firstName: 'John',
-                lastName: 'Doe',
-                userName: 'JohnDoe',
-                id: 123
-            }
-            return user;
+        options: {
+            handler: function (request, h) {
+                const user = {
+                    firstName: 'John',
+                    lastName: 'Doe',
+                    userName: 'JohnDoe',
+                    id: 123
+                }
+                return user;
+            },
+            description: 'Get object',
+            notes: 'Returns an object',
+            tags: ['api', 'object'] // ADD THIS TAG
         }
+        
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/objectnew',
+        options: {
+            handler: function (request, h) {
+                const user = {
+                    firstName: 'Jane',
+                    lastName: 'Doe',
+                    userName: 'JaneDoe',
+                    id: 321
+                }
+                return user;
+            },
+            description: 'Get object',
+            notes: 'Returns an object',
+            tags: ['api', 'object'] // ADD THIS TAG
+        }
+        
     });
 
     await server.start();
